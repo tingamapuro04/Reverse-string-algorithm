9062e325b512302579ad632e7865c7b643c47451
ADOYO ALPHONCE <alponcemobutu100@gmail.com>
2022-08-18 05:36:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function reverseString(str) {
-  let reversed = "";
-  for (let i = str.length-1; i >= 0; i--) {
-    reversed = reversed + str[i];
-  }
-  return reversed;
+  return str.split("").reduce((prev, curr) => {
+    prev = curr + prev;
+    return prev
+  }, "");
 }
